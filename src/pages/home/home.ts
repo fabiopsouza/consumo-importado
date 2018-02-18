588d74e314c1f26c41136c3e9af1179b42ef9cfa
DESKTOP-FFCVG9B\fabio <fabiosouza.web@gmail.com>
2018-02-18 13:04:52 -0300

    Iniciado construção do gráfico

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
 
 import { ModalController } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 import { FuelModalPage } from '../fuel-modal/fuel-modal';
 
@@ -10,10 +11,42 @@ import { FuelModalPage } from '../fuel-modal/fuel-modal';
 })
 export class HomePage {
 
+  @ViewChild('lineCanvas') lineCanvas;
+  lineChart: any;
+
+  kmlDates: string[];
+  kmlFuels: Number[];
+  km100lDates: string[];
+  km100lFuels: Number[];
+
   constructor(public modalCtrl: ModalController) {}
 
   openFuelModal(){
     let modal = this.modalCtrl.create(FuelModalPage);
     modal.present();
+  }
+
+  ionViewDidLoad() {
+    
+    //set graph Values
+
+    this.buildChart();
+  }
+
+  buildChart() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: this.kmlDates,
+        datasets: [{
+          label: "Consumo em Km/L",
+          fill: false,
+          backgroundColor: "#488aff",
+          borderColor: "#488aff",
+          data: this.kmlFuels
+        }]
+      },
+      options: { legend: { onClick: (e) => e.stopPropagation() } }
+    });
   }
 }
