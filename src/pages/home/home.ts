54f1e0d06a59a073cdee1f49adea1bb8d7ad4c39
DESKTOP-FFCVG9B\fabio <fabiosouza.web@gmail.com>
2018-02-19 22:05:28 -0300

    Iniciado lógica do filtro por segmento

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -27,6 +27,9 @@ export class HomePage {
   km100lDates: string[];
   km100lFuels: Number[];
 
+  startDate: Date = new Date();
+  endDate: Date = new Date();
+
   constructor(public modalCtrl: ModalController,
     public provider: FuelProvider,
     private datepipe: DatePipe) {
@@ -34,11 +37,25 @@ export class HomePage {
   }
 
   openFuelModal(){
+    
+    this.testSegment();
+
     let modal = this.modalCtrl.create(FuelModalPage);
-    modal.present();
+    //modal.present();
+  }
+
+  onChangeSegment(){
+    this.buildSegmentedGraph();
   }
 
   ionViewDidEnter() {
+    this.buildSegmentedGraph();
+  }
+
+  private buildSegmentedGraph(){
+    
+    this.setSegmentDates();
+
     let noReverse = false;
     this.provider.getAll(noReverse)
       .then((fuels) => {
@@ -47,6 +64,8 @@ export class HomePage {
         this.kmlFuels = [];
         this.km100lDates = [];
         this.km100lFuels = [];
+
+        fuels = this.getFiltred(fuels);
 
         fuels.forEach((fuel) => {
           this.kmlDates.push(this.getFormattedDate(fuel.date));
@@ -59,6 +78,27 @@ export class HomePage {
         this.buildChart('Consumo em Km/L', this.kmlChart, this.kmlCanvas, this.kmlDates, this.kmlFuels);
         this.buildChart('Autonomia em Km/100L', this.km100lChart, this.km100lCanvas, this.km100lDates, this.km100lFuels);
       });
+  }
+
+  private setSegmentDates(){
+    let factor;
+
+    if('week' == this.filter) factor = 7;
+    else if('month' == this.filter) factor = 31;
+    else factor = 365;
+
+    this.startDate.setDate(this.endDate.getDate() - factor);
+  }
+
+  private getFiltred(list: Fuel[]): Fuel[] {
+    
+    let filtred: Fuel[] = [];
+
+    list.forEach((item) => {
+      if(item.date > this.startDate && item.date < this.endDate) filtred.push(item);
+    });
+
+    return filtred;
   }
 
   private getFormattedDate(date): string{
@@ -82,16 +122,45 @@ export class HomePage {
     });
   }
 
-  test() {
-    for(let i = 0; i < 30; i++){
-      let f = new Fuel();
-      f.kml = this.randomNumber();
-      f.km100l = this.randomNumber();
-      f.date = this.randomDate(new Date(2018,0,1), new Date(2018,11,30));
+  //
+  // TESTS
+
+  testSegment(){
+    //week
+    let d1 = new Date(2018, 1, 18);
+    let d2 = new Date(2018, 1, 13);
+
+    let d3 = new Date(2018, 0, 29);
+    let d4 = new Date(2018, 1, 11);
+
+    let d5 = new Date(2017, 4, 23);
+    let d6 = new Date(2017, 10, 3);
+
+    let d7 = new Date(2015, 6, 1);
+    let d8 = new Date(2015, 7, 20);
     
-      console.log(f);
-      this.provider.save(f);
-    }
+    this.testNewFuel(d1);
+    this.testNewFuel(d2);
+    this.testNewFuel(d3);
+    this.testNewFuel(d4);
+    this.testNewFuel(d5);
+    this.testNewFuel(d6);
+    this.testNewFuel(d7);
+    this.testNewFuel(d8);
+  }
+
+  testNewFuel(date: Date) {
+    let f = new Fuel();
+    f.kml = this.randomNumber();
+    f.km100l = this.randomNumber();
+    f.date = date;
+  
+    console.log(f);
+    this.provider.save(f);
+  }
+
+  test() {
+    for(let i = 0; i < 30; i++) this.testNewFuel(this.randomDate(new Date(2018,0,1), new Date(2018,11,30)));
   }
 
   randomNumber(): number {
